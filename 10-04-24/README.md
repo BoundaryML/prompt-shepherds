c7261142eff7023894d14a4ed815c88065852b2b
hellovai <vbv@boundaryml.com>
2024-10-04 12:51:26 -0700

    Updates

--- 10-04-24/README.md
+++ 10-04-24/README.md
@@ -5,3 +5,11 @@
 1. Install UV - https://docs.astral.sh/uv/getting-started/installation/
 
 2. `uv sync`
+
+3. `uv run hello.py`
+
+## To run baml
+
+Follow installation instructions for your platform at https://docs.boundaryml.com/docs/get-started/quickstart/python
+
+VSCode install: https://docs.boundaryml.com/docs/get-started/quickstart/editors-vscode
--- 10-04-24/hello.py
+++ 10-04-24/hello.py
@@ -1,8 +1,65 @@
+from enum import Enum
+from typing import List
 from app import utils
+from baml_client import b
+from baml_client.types import Source
+
+
+def find_source(content: str) -> Source:
+    try:
+        return b.IdentifySource(content=content)
+    except Exception as e:
+        return Source.Other
+    
+class Fact:
+    def __init__(self, summary: str, citation: List[str], source_content: str):
+        self.summary = summary
+        self.citation = citation
+        self.is_reliable = [source in source_content for source in citation]
+
+    def __str__(self):
+        if not any(self.is_reliable):
+            return f"(Unreliable) {self.summary}"
+        return self.summary
+
+def extract_facts(content: str) -> list[Fact]:
+    response = b.ExtractFacts(content=content)
+    # prev = 0
+    # for response in stream:
+    #     if len(response.facts) > prev:
+    #         prev = len(response.facts)
+    #         print(f"Found {prev} facts")
+    # response = stream.get_final_response()
+    res = []
+    for fact in response.facts:
+        f = Fact(fact.summary, fact.citation, content)
+        if not all(f.is_reliable):
+            cirations = b.FindExtactCition(summary=f.summary, citation=f.citation)
+    return res
+
+def extract_opinions(content: str) -> list[str]:
+    return []
+
+def read_file(file_path: str) -> str:
+    with open(file_path, 'r') as f:
+        return f.read()
 
 def main():
-    print("Hello from 10-04-24!")
-    print(utils.foo())
+    content = read_file("tests/cnn1.txt")
+    source = find_source(content)
+    print(f"Article is from: {source.name}")
+
+    facts = extract_facts(content)
+    opinions = extract_opinions(content)
+
+    print(f"Facts are:")
+
+    for i, fact in enumerate(facts):
+        print(f"{i + 1}: {fact}")
+    print("------------------")
+    print("Opinions are:")
+    for i, opinion in enumerate(opinions):
+        print(f"{i + 1}: {opinion}")
 
 
 if __name__ == "__main__":
